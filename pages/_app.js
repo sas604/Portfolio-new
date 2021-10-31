9fd4f99ebad80b8345a685fea5c7a27749baea62
Aleksandr Tagunov <tagunovalexander@gmail.com>
2021-10-31 15:41:59 -0700

    Add Nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import NextNprogress from 'nextjs-progressbar';
 import Type from '../styles/Type';
 import GlobalStyles from '../styles/Global';
 import 'normalize.css';
@@ -5,6 +6,7 @@ import 'normalize.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNprogress color="#f5a53d" />
       <GlobalStyles />
       <Type />
       <Component {...pageProps} />
